ca9eb3b7c0005648884e030a191339d652e04798
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2024-05-05 00:50:06 +0400

    add uninstall option && pixi install folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 __file__=${BASH_SOURCE}
 __root__=$(realpath $(dirname ${__file__}))
 
-PIXI_HOME=${HOME}/.pixi
+PIXI_HOME=${PIXI_HOME:-"$HOME/.pixi"}
 
 LOCAL_BIN_ROOT=${__root__}/bin
 
@@ -10,7 +10,15 @@ HOME_CONFIG_ROOT=${HOME}/.config
 
 SHELL_NAME=$(basename ${SHELL})
 case ${SHELL_NAME} in
-bash | zsh)
+bash)
+	if [ -f ${HOME}/.bash_profile ]; then
+		SHELL_RC=${HOME}/.${SHELL_NAME}_profile
+	else
+		SHELL_RC=${HOME}/.${SHELL_NAME}rc
+	fi
+	;;
+
+zsh)
 	SHELL_RC=${HOME}/.${SHELL_NAME}rc
 	;;
 
@@ -53,16 +61,15 @@ on_setup_begin() {
 }
 
 setup_pixi() {
-	echo -n "   pixi ... "
+	echo -n "   pixi [${PIXI_HOME}] ... "
 
-	if [ ! $"(which pixi &> /dev/null)" ]; then
-		export PIXI_HOME=${PIXI_HOME} curl -fsSL https://pixi.sh/install.sh | ${SHELL}
+	if ! command -v pixi &>/dev/null; then
+		export PIXI_HOME=${PIXI_HOME} PIXI_NO_PATH_UPDATE=1 && curl -fsSL https://pixi.sh/install.sh | ${SHELL}
 	fi
 
 	echo '''
-  # Pixi setup 
-  export PIXI_HOME='${PIXI_HOME}'
-  export PATH=${PIXI_HOME}/bin:${PATH}
+  # Pixi setup
+  export PATH='${PIXI_HOME}'/bin:${PATH}
 
   case ${SHELL} in
     *bash*|*zsh*)
@@ -75,7 +82,7 @@ setup_pixi() {
 }
 
 pixi_install_packages() {
-	${PIXI_HOME}/pixi/bin global install -q ${1} &>/dev/null
+	${PIXI_HOME}/bin/pixi global install -q ${1} &>/dev/null
 }
 
 setup_essentials() {
@@ -83,7 +90,7 @@ setup_essentials() {
 	pixi_install_packages "fd-find ripgrep lsdeluxe zoxide"
 
 	echo '''
-  # Essentials setup  
+  # Essentials setup
   alias ls=lsd
 
   case ${SHELL} in
@@ -133,21 +140,67 @@ setup_neovim() {
 on_setup_end() {
 	local shell_rc_init_cmd="source '${DOTFILES_RC}'"
 	if grep -qF "${shell_rc_init_cmd}" "${SHELL_RC}"; then
-		echo "System config ${SHELL_RC} already contains dotfile initialization"
+		echo "System config at ${SHELL_RC} already contains dotfile initialization"
 	else
 		echo ${shell_rc_init_cmd} >>${SHELL_RC}
 	fi
 
+	echo '`source '${SHELL_RC}'` might be required for changes to take place'
 	echo "All done"
 }
 
-main() {
+un_setup() {
+	echo Uninstall will remove global pixi installation at ${PIXI_HOME} \
+		and delete dotfiles settings sourcing from ${SHELL_RC}
+	echo
+
+	while true; do
+		read -p "Shall we proceed? [yn] " confirmation
+		case ${confirmation} in
+		y)
+			break
+			;;
+
+		n)
+			echo Uninstallation aborted
+			exit 0
+			;;
+
+		*)
+			echo Unsupported option '`'${confirmation}'`', expected '`[yn]`'
+			continue
+			;;
+		esac
+	done
+
+	local shell_rc_init_cmd="source '${DOTFILES_RC}'"
+	grep -v "${shell_rc_init_cmd}" "${SHELL_RC}" >"${SHELL_RC}.bak" &&
+		mv "${SHELL_RC}.bak" "${SHELL_RC}"
+
+	unalias ls cd vsc &>/dev/null
+
+	rm -fr ${PIXI_HOME}
+
+	echo '`source '${SHELL_RC}'` might be required for changes to take place'
+	echo "All done"
+}
+
+case $1 in
+install)
 	on_setup_begin &&
 		setup_pixi &&
 		setup_essentials &&
 		setup_starship &&
 		setup_neovim &&
 		on_setup_end
-}
+	;;
 
-main
+uninstall)
+	un_setup
+	;;
+
+*)
+	echo Argument could be either $(install) or $(uninstall), got $(${1})
+	exit 1
+	;;
+esac
