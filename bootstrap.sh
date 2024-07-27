1660b7fc8ae69369da67128d745f9daba381f8dc
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2024-07-27 23:22:35 +0400

    add setup macos to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,47 +11,47 @@ HOME_CONFIG_ROOT=${HOME}/.config
 SHELL_NAME=$(basename ${SHELL})
 case ${SHELL_NAME} in
 bash)
-	if [ -f ${HOME}/.bash_profile ]; then
-		SHELL_RC=${HOME}/.${SHELL_NAME}_profile
-	else
-		SHELL_RC=${HOME}/.${SHELL_NAME}rc
-	fi
-	;;
+  if [ -f ${HOME}/.bash_profile ]; then
+    SHELL_RC=${HOME}/.${SHELL_NAME}_profile
+  else
+    SHELL_RC=${HOME}/.${SHELL_NAME}rc
+  fi
+  ;;
 
 zsh)
-	SHELL_RC=${HOME}/.${SHELL_NAME}rc
-	;;
+  SHELL_RC=${HOME}/.${SHELL_NAME}rc
+  ;;
 
 *)
-	echo Shell ${SHELL_NAME} is unsupported
-	exit 1
-	;;
+  echo Shell ${SHELL_NAME} is unsupported
+  exit 1
+  ;;
 esac
 
 DOTFILES_RC=${__root__}/generated-dotfiles-rc/${SHELL_NAME}.sh
 
 on_setup_begin() {
-	echo "Setting up dotfiles"
+  echo "Setting up dotfiles"
 
-	mkdir -p "$(dirname ${DOTFILES_RC})"
+  mkdir -p "$(dirname ${DOTFILES_RC})"
 
-	if [ -f ${DOTFILES_RC} ]; then
-		truncate -s 0 ${DOTFILES_RC}
-	else
-		touch ${DOTFILES_RC}
-	fi
+  if [ -f ${DOTFILES_RC} ]; then
+    truncate -s 0 ${DOTFILES_RC}
+  else
+    touch ${DOTFILES_RC}
+  fi
 
-	local filename=()
-	for filename in $(ls -1 "${LOCAL_BIN_ROOT}"); do
-		chmod +x ${LOCAL_BIN_ROOT}/${filename}
-	done
+  local filename=()
+  for filename in $(ls -1 "${LOCAL_BIN_ROOT}"); do
+    chmod +x ${LOCAL_BIN_ROOT}/${filename}
+  done
 
-	for filename in $(ls -1 "${LOCAL_CONFIG_ROOT}"); do
-		rm -rf ${HOME_CONFIG_ROOT}/${filename}
-		ln -sf ${LOCAL_CONFIG_ROOT}/${filename} ${HOME_CONFIG_ROOT}/${filename}
-	done
+  for filename in $(ls -1 "${LOCAL_CONFIG_ROOT}"); do
+    rm -rf ${HOME_CONFIG_ROOT}/${filename}
+    ln -sf ${LOCAL_CONFIG_ROOT}/${filename} ${HOME_CONFIG_ROOT}/${filename}
+  done
 
-	echo '''
+  echo '''
   export PATH="'${LOCAL_BIN_ROOT}':${PATH}"
 
   for vsc in code code-insiders; do
@@ -62,49 +62,49 @@ on_setup_begin() {
   done
   ''' >>${DOTFILES_RC}
 
-	case ${SHELL_NAME} in
-	zsh)
-		echo '''
+  case ${SHELL_NAME} in
+  zsh)
+    echo '''
       autoload -Uz compinit
       compinit
       ''' >>${DOTFILES_RC}
-		;;
-	esac
+    ;;
+  esac
 }
 
 setup_pixi() {
-	echo -n "   pixi [${PIXI_HOME}] ... "
+  echo -n "   pixi [${PIXI_HOME}] ... "
 
-	if ! command -v pixi &>/dev/null; then
-		export PIXI_HOME=${PIXI_HOME} PIXI_NO_PATH_UPDATE=1 && curl -fsSL https://pixi.sh/install.sh | ${SHELL}
-	fi
+  if ! command -v pixi &>/dev/null; then
+    export PIXI_HOME=${PIXI_HOME} PIXI_NO_PATH_UPDATE=1 && curl -fsSL https://pixi.sh/install.sh | ${SHELL}
+  fi
 
-	echo '''
+  echo '''
   # Pixi setup
   export PIXI_HOME='${PIXI_HOME}'
   export PATH='${PIXI_HOME}'/bin:${PATH}
   ''' >>${DOTFILES_RC}
 
-	local cmd=""
-	case ${SHELL_NAME} in
-	bash | zsh)
-		cmd='eval "$(pixi completion --shell '${SHELL_NAME}')"'
-		;;
-	esac
-	echo ${cmd} >>${DOTFILES_RC}
+  local cmd=""
+  case ${SHELL_NAME} in
+  bash | zsh)
+    cmd='eval "$(pixi completion --shell '${SHELL_NAME}')"'
+    ;;
+  esac
+  echo ${cmd} >>${DOTFILES_RC}
 
-	echo "Done"
+  echo "Done"
 }
 
 pixi_install_packages() {
-	${PIXI_HOME}/bin/pixi global install -q ${1} &>/dev/null
+  ${PIXI_HOME}/bin/pixi global install -q ${1} &>/dev/null
 }
 
 setup_essentials() {
-	echo -n "   essentials ... "
-	pixi_install_packages "fd-find ripgrep lsdeluxe zoxide nvim"
+  echo -n "   essentials ... "
+  pixi_install_packages "fd-find ripgrep lsdeluxe zoxide nvim"
 
-	echo '''
+  echo '''
   # Essentials setup
   alias ls=lsd
   alias l='"ls -l"'
@@ -112,139 +112,145 @@ setup_essentials() {
   alias cd=z
   ''' >>${DOTFILES_RC}
 
-	case ${SHELL_NAME} in
-	bash)
-		pixi_install_packages "bash-completion"
-		echo '''
+  case ${SHELL_NAME} in
+  bash)
+    pixi_install_packages "bash-completion"
+    echo '''
     eval "$(zoxide init '${SHELL_NAME}')"
 
     . ${PIXI_HOME}/envs/bash-completion/share/bash-completion/bash_completion
     ''' >>${DOTFILES_RC}
-		;;
-	zsh)
-		echo 'eval "$(zoxide init '${SHELL_NAME}')"' >>${DOTFILES_RC}
-		;;
-	esac
+    ;;
+  zsh)
+    echo 'eval "$(zoxide init '${SHELL_NAME}')"' >>${DOTFILES_RC}
+    ;;
+  esac
 
-	echo "Done"
+  echo "Done"
 }
 
 setup_starship() {
-	echo -n "   starship ... "
-	pixi_install_packages starship
+  echo -n "   starship ... "
+  pixi_install_packages starship
 
-	local cmd=""
-	case ${SHELL_NAME} in
-	bash | zsh)
-		cmd='eval "$(starship init '${SHELL_NAME}')"'
-		;;
-	esac
+  local cmd=""
+  case ${SHELL_NAME} in
+  bash | zsh)
+    cmd='eval "$(starship init '${SHELL_NAME}')"'
+    ;;
+  esac
 
-	echo '''
+  echo '''
   # Starship setup
   if [[ ${TERM_PROGRAM} != "WarpTerminal" ]]; then
     '${cmd}'
   fi
   ''' >>${DOTFILES_RC}
 
-	echo "Done"
+  echo "Done"
 }
 
 setup_goodies() {
-	echo -n "   goodies ... "
-	pixi_install_packages ruff
-	echo "Done"
+  echo -n "   goodies ... "
+  pixi_install_packages ruff
+  echo "Done"
 }
 
 setup_tmux() {
-	echo -n "   tmux ... "
-	pixi_install_packages tmux
+  echo -n "   tmux ... "
+  pixi_install_packages tmux
 
-	local tpm_root=${HOME}/.tmux/plugins/tpm
-	if [ -d ${tpm_root} ]; then
-		rm -rf ${tpm_root}
-	fi
-	git clone -q https://github.com/tmux-plugins/tpm ${tpm_root}
-	bash ${tpm_root}/bin/install_plugins &>/dev/null
+  local tpm_root=${HOME}/.tmux/plugins/tpm
+  if [ -d ${tpm_root} ]; then
+    rm -rf ${tpm_root}
+  fi
+  git clone -q https://github.com/tmux-plugins/tpm ${tpm_root}
+  bash ${tpm_root}/bin/install_plugins &>/dev/null
 
-	echo "Done"
+  echo "Done"
 }
 
 on_setup_end() {
-	local shell_rc_init_cmd="source '${DOTFILES_RC}'"
-	if grep -qF "${shell_rc_init_cmd}" "${SHELL_RC}"; then
-		echo "System config at ${SHELL_RC} already contains dotfile initialization"
-	else
-		echo ${shell_rc_init_cmd} >>${SHELL_RC}
-	fi
+  local shell_rc_init_cmd="source '${DOTFILES_RC}'"
+  if grep -qF "${shell_rc_init_cmd}" "${SHELL_RC}"; then
+    echo "System config at ${SHELL_RC} already contains dotfile initialization"
+  else
+    echo ${shell_rc_init_cmd} >>${SHELL_RC}
+  fi
 
-	echo '`source '${SHELL_RC}'` might be required for changes to take place'
-	echo "All done"
+  echo '`source '${SHELL_RC}'` might be required for changes to take place'
+  echo "All done"
 }
 
 install() {
-	on_setup_begin &&
-		setup_pixi &&
-		setup_essentials &&
-		setup_starship &&
-		setup_goodies &&
-		setup_tmux &&
-		on_setup_end
+  on_setup_begin &&
+    setup_pixi &&
+    setup_essentials &&
+    setup_starship &&
+    setup_goodies &&
+    setup_tmux &&
+    on_setup_end
+
+  case ${OSTYPE} in
+  darwin*)
+    $SHELL macos/setup
+    ;;
+  esac
 }
 
 uninstall() {
-	echo Uninstall will remove global pixi installation at ${PIXI_HOME} \
-		and delete dotfiles settings sourcing from ${SHELL_RC}
-	echo
+  echo Uninstall will remove global pixi installation at ${PIXI_HOME} \
+    and delete dotfiles settings sourcing from ${SHELL_RC}
+  echo
 
-	while true; do
-		read -p "Shall we proceed? [yn] " confirmation
-		case ${confirmation} in
-		y)
-			break
-			;;
+  while true; do
+    read -p "Shall we proceed? [yn] " confirmation
+    case ${confirmation} in
+    y)
+      break
+      ;;
 
-		n)
-			echo Uninstallation aborted
-			exit 0
-			;;
+    n)
+      echo Uninstallation aborted
+      exit 0
+      ;;
 
-		*)
-			echo Unsupported option '`'${confirmation}'`', expected '`[yn]`'
-			continue
-			;;
-		esac
-	done
+    *)
+      echo Unsupported option '`'${confirmation}'`', expected '`[yn]`'
+      continue
+      ;;
+    esac
+  done
 
-	local shell_rc_init_cmd="source '${DOTFILES_RC}'"
-	grep -v "${shell_rc_init_cmd}" "${SHELL_RC}" >"${SHELL_RC}.bak" &&
-		mv "${SHELL_RC}.bak" "${SHELL_RC}"
+  local shell_rc_init_cmd="source '${DOTFILES_RC}'"
+  grep -v "${shell_rc_init_cmd}" "${SHELL_RC}" >"${SHELL_RC}.bak" &&
+    mv "${SHELL_RC}.bak" "${SHELL_RC}"
 
-	(cat "${DOTFILES_RC}" |
-		grep alias |
-		awk -F "=" '{print $1}' |
-		awk -F " " '{print $2}' |
-		xargs unalias) &>/dev/null
+  (cat "${DOTFILES_RC}" |
+    grep alias |
+    awk -F "=" '{print $1}' |
+    awk -F " " '{print $2}' |
+    xargs unalias) &>/dev/null
 
-	for filename in $(ls -1 "${LOCAL_CONFIG_ROOT}"); do
-		rm -rf ${HOME_CONFIG_ROOT}/${filename}
-	done
+  for filename in $(ls -1 "${LOCAL_CONFIG_ROOT}"); do
+    rm -rf ${HOME_CONFIG_ROOT}/${filename}
+  done
 
-	echo '`source '${SHELL_RC}'` might be required for changes to take place'
-	echo "All done"
+  echo '`source '${SHELL_RC}'` might be required for changes to take place'
+  echo "All done"
 }
 
 case $1 in
 install)
-	install
-	;;
+  install
+  ;;
 
 uninstall)
-	uninstall
-	;;
+  uninstall
+  ;;
 
 *)
-	echo Argument could be either '`install`' or '`uninstall`', got '`'${1}'`'
-	exit 1
-	;;
+  echo Argument could be either '`install`' or '`uninstall`', got '`'${1}'`'
+  exit 1
+  ;;
 esac
