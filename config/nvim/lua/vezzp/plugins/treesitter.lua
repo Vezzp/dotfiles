4150b51ec9c2977483c6ded89b6082c0bbfbc037
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2024-10-18 15:19:36 +0300

    add treesitter text objects

--- config/nvim/lua/vezzp/plugins/treesitter.lua
+++ config/nvim/lua/vezzp/plugins/treesitter.lua
@@ -2,45 +2,47 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-
-    treesitter.setup({
-      modules = {},
-      auto_install = true,
-      ignore_install = {},
-      sync_install = false,
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "json",
-        "yaml",
-        "html",
-        "toml",
-        "markdown",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "c",
-        "cpp",
-        "cuda",
-        "rust",
-        "go",
-        "gomod",
-        "make",
-        "cmake",
-        "python",
-        "latex",
-        "proto",
-        "bash",
-      },
-    })
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  opts = {
+    modules = {},
+    auto_install = true,
+    ignore_install = {},
+    sync_install = false,
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+    autotag = {
+      enable = true,
+    },
+    ensure_installed = {
+      "json",
+      "yaml",
+      "html",
+      "toml",
+      "markdown",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
+      "c",
+      "cpp",
+      "cuda",
+      "rust",
+      "go",
+      "gomod",
+      "make",
+      "cmake",
+      "python",
+      "latex",
+      "proto",
+      "bash",
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
