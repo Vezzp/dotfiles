a65d906150feb0e217cb8ba298db3e1f7f3c5542
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2025-04-28 07:14:34 +0000

    use more catppuccin

--- config/nvim/lua/vezzp/plugins/colorscheme.lua
+++ config/nvim/lua/vezzp/plugins/colorscheme.lua
@@ -4,13 +4,23 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "auto",
+      term_colors = true,
+      transparent_background = true,
       background = {
         light = "latte",
         dark = "frappe",
       },
       integrations = {
-        noice = true,
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        harpoon = true,
         telescope = true,
+        mason = true,
+        noice = true,
+        notify = true,
+        which_key = true,
+        fidget = true,
         nvimtree = true,
       },
     })
