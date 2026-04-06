bb3b81d5320050674ce7f0c0ce41450fde6cb65d
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2026-04-06 14:01:29 +0400

    add snacks

---
+++ config/nvim/lua/vezzp/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    input = {
+      enabled = true,
+      win = {
+        style = "input",
+        relative = "editor",
+        row = 0.5,
+        col = 0.5,
+      },
+    },
+  },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    local group = vim.api.nvim_create_augroup("VezzpSnacksInput", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = group,
+      pattern = "snacks_input",
+      callback = function()
+        vim.schedule(function()
+          if vim.bo.buftype == "prompt" then
+            vim.cmd("stopinsert")
+          end
+        end)
+      end,
+    })
+  end,
+}
